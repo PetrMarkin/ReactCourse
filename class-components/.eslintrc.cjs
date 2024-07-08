c0e48b569c19c83a3d470413fe13c03b4d684820
Markin Petr <skyhome5041@icloud.com>
2024-07-08 16:30:17 +0300

    chore: update build

--- class-components/.eslintrc.cjs
+++ class-components/.eslintrc.cjs
@@ -1,3 +1,5 @@
+const react = require('eslint-plugin-react');
+
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
@@ -8,7 +10,8 @@ module.exports = {
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh', 'react-compiler', 'prettier'],
+  files: ['**/*.{js,jsx,mjs,cjs,ts,tsx}'],
+  plugins: ['react-refresh', 'react-compiler', 'prettier', react],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
